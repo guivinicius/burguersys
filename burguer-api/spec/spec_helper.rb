99871a50c66b4f1f9df20de66b74a00a41629909
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-05-07 11:44:45 -0300

    Adding essential files

---
+++ burguer-api/spec/spec_helper.rb
@@ -0,0 +1,34 @@
+require 'simplecov'
+SimpleCov.start('rails') do
+  add_group "Services", "app/services"
+end
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.order = "random"
+
+  # FactoryGirl
+  config.include FactoryGirl::Syntax::Methods
+
+  # Database cleaner configuration
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
