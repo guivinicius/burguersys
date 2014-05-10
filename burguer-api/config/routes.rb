3ec384be8973dcafac61edd52c6cb3fc1178a2ad
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-05-10 16:23:16 -0300

    Addin tables API

--- burguer-api/config/routes.rb
+++ burguer-api/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'tables/index'
+
   resources :orders, except: [:new, :edit]
   resources :products, except: [:new, :edit]
+  resources :tables, only: [:index]
 
   root 'products#index'
 end
---
+++ burguer-api/spec/requests/tables_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'Products API' do
+  let!(:table1) { create(:table, number: 1) }
+  let!(:table2) { create(:table, number: 2) }
+
+  describe 'GET /tables' do
+    before do
+      get '/tables'
+    end
+
+    it 'returns status 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns all tables' do
+      expect(json.length).to eq(2)
+    end
+  end
+end
