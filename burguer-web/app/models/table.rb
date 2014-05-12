890df93caffbfe1641eb4a6948599c6b3ef9dd08
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-05-12 15:07:21 -0300

    Setting up project

---
+++ burguer-web/app/models/table.rb
@@ -0,0 +1,3 @@
+class Table < ActiveResource::Base
+  self.site = 'http://localhost:3000/api/tables'
+end
--- burguer-web/spec/spec_helper.rb
+++ burguer-web/spec/spec_helper.rb
@@ -2,12 +2,21 @@ require 'simplecov'
 SimpleCov.start('rails')
 
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
+
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'webmock/rspec'
+require 'vcr'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'vcr_cassettes'
+  c.hook_into :webmock
+end
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
