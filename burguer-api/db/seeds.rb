02a7d8c6ab5c743d8ada96231ef8cdbd0d7cba83
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-05-12 22:33:05 -0300

    creating some data

--- burguer-api/db/seeds.rb
+++ burguer-api/db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+tables = [
+  { number: 1 },
+  { number: 2 },
+  { number: 3 },
+  { number: 4 }
+]
+Table.create(tables)
+
+products = [
+  { name: 'SuperDuper Meat', price: 9.90 },
+  { name: 'SuperDuper Revenge', price: 12.90 },
+  { name: 'Not a MC something', price: 11.90 },
+  { name: 'Coca-bola', price: 4.90 },
+  { name: 'Peptosi', price: 3.99 },
+  { name: 'DJTea', price: 2.90 }
+]
+Product.create(products)
+
+partners = [
+  { name: 'Monster Snack' },
+  { name: 'Gudi Lanche' }
+]
+Partner.create(partners)
